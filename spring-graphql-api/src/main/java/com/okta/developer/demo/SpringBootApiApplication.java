c9c83fb72c1a74dad4ef36bf93c3da89350fa9cb
indiepopart <jimena@gmail.com>
2024-01-19 18:21:38 -0300

    update tabs to spaces

--- spring-graphql-api/src/main/java/com/okta/developer/demo/SpringBootApiApplication.java
+++ spring-graphql-api/src/main/java/com/okta/developer/demo/SpringBootApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiApplication.class, args);
+    }
 
 }
